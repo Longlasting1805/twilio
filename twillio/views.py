1a6ccb22e48357032a7e9e68856111ea8808764c
Akande Al-ameen <longlasting1805@gmail.com>
2023-11-07 17:13:31 +0100

    first commit

---
+++ twillio/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render
+
+# Create your views here.
+import json
+import requests
+from django.http import JsonResponse
+from twilio.twiml.voice_response import Dial, Gather, Play, Pause, Record, Redirect, Stream
+
+def handle_incoming_call(request):
+    # Retrieve user location from the request body
+    user_location = json.loads(requests.body.decode('utf-8'))['userLocation']
+
+    # Get navigation instructions using a mapping API
+    navigation_instructions = get_navigation_instructions(user_location)
+
+    # Initialize Twiml response
+    response = Dial()
+
+
+
+    # Process navigation instructions and generate Twiml prompts
+    for instruction in navigation_instructions:
+        if instruction['type'] == 'intersection':
+            handle_intersection(response, instruction)
+        elif instruction['type'] == 'curb':
+            handle_curb(response, instruction)
+        elif instruction['type'] == 'stairs':
+            handle_stairs(response, instruction)
+        elif instruction['type'] == 'pedestrianCrosswalk':
+            handle_pedestrian_crosswalk(response, instruction)
+        elif instruction['type'] == 'sidewalkObstacle':
+            handle_sidewalk_obstacle(response, instruction)
+        elif instruction['type'] == 'elevator':
+            handle_elevator(response, instruction)
+        elif instruction['type'] == 'escalator':
+            handle_escalator(response, instruction)
+        elif instruction['type'] == 'streetCorner':
+            handle_street_corner(response, instruction)
+        elif instruction['type'] == 'publicTransportationStop':
+            handle_public_transportation_stop(response, instruction)
+        elif instruction['type'] == 'pedestrianSignal':
+            handle_pedestrian_signal(response, instruction)
+        elif instruction['type'] == 'storeEntrance':
+            handle_store_entrance(response, instruction)
+        elif instruction['type'] == 'crowdedArea':
+            handle_crowded_area(response, instruction)
+
+    # Return the Twiml response
+    return JsonResponse({'twiml': str(response)})
+
+def get_navigation_instructions(user_location):
+    # Implement logic to retrieve navigation instructions from a mapping API
+    # For this simplified example, we'll assume navigation instructions are available
+    return [
+        {
+            "type": "intersection",
+            "description": "Turn left at the next intersection."
+        },
+        {
+            "type": "curb",
+            "description": "Be cautious of a curb ahead."
+        },
+        {
+            "type": "stairs",
+            "description": "Beware of a flight of stairs ahead."
+        },
+        {
+            "type": "pedestrianCrosswalk",
+            "description": "Approach a pedestrian crosswalk. Wait for the signal to cross safely."
+        },
+        {
+            "type": "sidewalkObstacle",
+            "description": "Watch out for a sidewalk obstacle ahead."
+        },
+        {
+            "type": "elevator",
+            "description": "You are approaching an elevator. Press the button to call for the elevator."
+        },
+        {
+            "type": "escalator",
+            "description": "Take care while using the escalator."
+        },
+        {
+            "type": "streetCorner",
+            "description": "You are approaching a street corner. Proceed with caution."
+        },
+        {
+            "type": "publicTransportationStop",
+            "description": "You are approaching a public transportation stop. Check for your desired route."
+        },
+        {
+            "type": "pedestrianSignal",
+            "description": "Wait for the pedestrian signal to cross safely."
+        },
+        {
+            "type": "storeEntrance",
+            "description": "You are approaching a store entrance. The entrance is located on the left."
+        },
+        {
+            "type": "crowdedArea",
+            "description": "Be aware of the crowded area ahead. Move cautiously."
+        }
+    ]
+
+def handle_intersection(response, instruction):
+    response.append(Play(audio_url='https://demo.twilio.com/docs/voice/tutorial/audio-prompts.xml'))
+
+def handle_curb(response):
+    response.append()    
+
